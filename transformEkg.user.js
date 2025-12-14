c251c112a2ed36e4231b69e23d4808fee15720fc
Suttisak Denduangchai <suttisak.luke@outlook.com>
2025-12-14 15:48:56 +0700

    first commit

---
+++ transformEkg.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @namespace     https://openuserjs.org/users/lukespacewalker
+// @name          Transform EKG
+// @description   Add copy EKG butto on ebmc.bdms.ac.th
+// @copyright     2025, lukespacewalker (https://openuserjs.org/users/lukespacewalker)
+// @license       MIT
+// @version       1.0.0
+// @include       https://www.example.com/*
+// @grant none
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author lukespacewalker
+// ==/OpenUserJS==
+
+/**
+  *
+  * Please begin typing or paste your User script now.
+  *
+  * NOTE: It is still strongly recommended to use the Author Tools panel to
+  *       add your `@updateURL` even if we are not in lockdown.
+  *
+  */
