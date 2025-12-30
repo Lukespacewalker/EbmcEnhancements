8d8921fc1cc02e8f0c791cd032d035e80b602845
Suttisak Denduangchai <suttisak.lukesp@outlook.com>
2025-12-30 17:58:59 +0700

    Update docscan.user.js

--- docscan.user.js
+++ docscan.user.js
@@ -5,18 +5,90 @@
 // @description   Multiple enhancements for Docscan
 // @copyright     2025, Suttisak Denduangchai (https://github.com/lukespacewalker)
 // @license       MIT
-// @version       1.0.1
+// @version       1.0.3
 // @include       https://dscanweb.bdms.co.th/*
 // @grant         GM_addStyle
+// @grant         GM.xmlHttpRequest
+// @connect       dscanweb.bdms.co.th
+// @require       https://cdn.jsdelivr.net/npm/@trim21/gm-fetch@0.3.0
+// @downloadURL https://update.greasyfork.org/scripts/559649/Docscan%20Enhancement.user.js
+// @updateURL https://update.greasyfork.org/scripts/559649/Docscan%20Enhancement.meta.js
 // ==/UserScript==
+
+function addStyles() {
+    'use strict';
+
+    GM_addStyle(`
+#search .left-content {
+height: calc(100dvh - 41.47px);
+width: 240px;
+display:flex;
+flex-direction: column;
+}
+
+#content{
+padding-left:240px;
+}
+
+#search .tab-content{
+flex: 1 0 auto;
+overflow-y:auto;
+overflow-x:hidden;
+    white-space: unset;
+}
+    `);
+}
+
 
 function removeTimeout() {
     window.clearInterval(unsafeWindow.timer)
     unsafeWindow.timer = null
 }
 
+function removeTrash(){
+    document.querySelector(".image-wrapper").remove()
+
+    const targetElement = document.querySelector(".tab-content");
+
+const observer = new MutationObserver(function(mutationsList) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+            const currentHeight = mutation.target.style.height;
+            const currentOverflow = mutation.target.style.overflow;
+            if (currentHeight) {
+                targetElement.style.removeProperty("height")
+            }
+            if (currentOverflow) {
+                targetElement.style.removeProperty("overflow")
+            }
+        }
+    }
+});
+
+// Configuration of the observer:
+const config = {
+    attributes: true,
+    attributeFilter: ['style'] // Only observe the 'style' attribute
+};
+
+// Start observing the target element
+observer.observe(targetElement, config);
+}
+
+var fetch_timer = null
+
+async function updateSessionCookie(){
+    window.clearTimeout(fetch_timer)
+    unsafeWindow.timer = window.setTimeout(updateSessionCookie, 1 + Math.random() * 1000 * 60 * 10)
+
+    const res = await GM_fetch("https://dscanweb.bdms.co.th/main", { method: "GET" });
+}
+
 function main() {
     removeTimeout();
+    removeTrash();
+    addStyles();
+    updateSessionCookie();
 }
 
 /*
@@ -27,3 +99,4 @@ if (document.readyState === "loading") {
 } else {
     main();
 }
+ห
